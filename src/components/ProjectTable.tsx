b9f39bc818088842cffb3f526d247fcc13c197bd
Ignacio Dotta <ij.dotta@gmail.com>
2025-09-22 20:37:12 -0300

    update table

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -1,9 +1,18 @@
-import type { ProjectResponse } from "@/types/ProjectResponse.ts";
 import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import type {Person} from "@/types/Person.ts";
+import type {Project} from "@/types/Project.ts";
 
 type ProjectTableProps = {
-  projects: ProjectResponse[];
+  projects: Project[];
 };
+
+const mapPersonToString = (person: Person): string => {
+  return `${person.lastname}, ${person.name}`;
+}
+
+const mapParticipantsToCellString = (participants: Person[]): string => {
+  return participants.map(mapPersonToString).join("; ");
+}
 
 export function ProjectTable({ projects }: ProjectTableProps) {
   return (
@@ -16,7 +25,12 @@ export function ProjectTable({ projects }: ProjectTableProps) {
               <TableHead>Título</TableHead>
               <TableHead>Tipo</TableHead>
               <TableHead>Subtipos</TableHead>
-              <TableHead>Participantes</TableHead>
+              <TableHead>Directores</TableHead>
+              <TableHead>Co-directores</TableHead>
+              <TableHead>Colaboradores</TableHead>
+              <TableHead>Alumnos</TableHead>
+              <TableHead>Estado</TableHead>
+              <TableHead>Acciones</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -25,7 +39,22 @@ export function ProjectTable({ projects }: ProjectTableProps) {
                 <TableCell>{project.title}</TableCell>
                 <TableCell>{project.type}</TableCell>
                 <TableCell>{project.subtypes?.join(", ")}</TableCell>
-                <TableCell>{project.participants?.map(p => p.person.name + " " + p.person.lastname).join("; ")}</TableCell>
+                <TableCell></TableCell>
+                <TableCell>{mapParticipantsToCellString(project.directors)}</TableCell>
+                <TableCell>{mapParticipantsToCellString(project.codirectors)}</TableCell>
+                <TableCell>{mapParticipantsToCellString(project.collaborators)}</TableCell>
+                <TableCell>{mapParticipantsToCellString(project.students)}</TableCell>
+                <TableCell>{project.completion ? "Finalizado" : "En curso"}</TableCell>
+                <TableCell>
+                  <a
+                    href={`https://repositorio.utn.edu.ar/handle/123456789/${project.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-600 hover:underline"
+                  >
+                    Ver en Repositorio
+                  </a>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
